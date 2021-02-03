b44d74497f436d9451ecb12eb33184b806eff65d
Amit Dilip Gujar <amithero3342@gmail.com>
2021-02-04 02:31:39 +0530

    routes updated with controllers

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,127 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
-const checkAuth = require("../middleware/check-auth")
+const checkAuth = require("../middleware/check-auth");
 const Order = require("../models/order");
 const Product = require("../models/product");
+const OrderController = require("../controllers/orders");
 
 // view all orders
-router.get("/", checkAuth, (req, res, next) => {
-    Order.find()
-    .select("_id product quantity")
-    .populate("product", "name")
-    .exec()
-    .then(docs =>{
-        res.status(200).json({
-            count: docs.length,
-            orders: docs.map(doc => {
-              return {
-                _id: doc._id,
-                product: doc.product,
-                quantity: doc.quantity,
-                request: {
-                    type: "GET",
-                    url: "http://localhost:3000/orders/" + doc._id
-                }
-              }  
-            })
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.get("/", checkAuth, OrderController.orders_get_all);
 // create order
-router.post("/", checkAuth, (req, res, next) => {
-    Product.findById(req.body.productId)
-    .then(product => {
-        if (!product) {
-            return res.status(404).json({
-                message:"product not found"
-            });
-        }
-        const order = new Order({
-            _id: mongoose.Types.ObjectId(),
-            quantity: req.body.quantity,
-            product: req.body.productId, 
-        });
-        return order.save()
-    })
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message:"Order created successfully",
-            createdOrder: {
-                _id: result._id,
-                quantity: result.quantity,
-                product: result.product
-            },
-            request: {
-                type:"GET",
-                url:"http://localhost:3000/orders/" + result._id
-            }
-        });
-    }) 
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.post("/", checkAuth, OrderController.orders_create_order);
 // get individual order
-router.get("/:orderId", checkAuth, (req, res, next) => {
-    Order.findById(req.params.orderId)
-    .populate("product")
-    .exec()
-    .then(order => {
-        if (order === null) {
-            return res.status(404).json({
-                message:"order not found"
-            });
-        }
-        res.status(200).json({
-            order: order,
-            request: {
-                type: "GET",
-                url: "http://localhost:3000/orders/",
-                description: "get all orders"
-            }
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        })
-    })
-});
+router.get("/:orderId", checkAuth, OrderController.orders_get_order);
 // delete orders
-router.delete("/:orderId", checkAuth, (req, res, next) => {
-    Order.remove({_id: req.params.orderId})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message:"Order deleted successfully",
-            request: {
-                type: "POST",
-                url: "http://localhost:3000/orders/",
-                description: "create new order",
-                body : {
-                    productId:  "ID",
-                    quantity: "Number"
-                }
-            }
-        });
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.delete("/:orderId", checkAuth, OrderController.orders_delete_order);
 
 module.exports = router;
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,104 +3,16 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const UserController = require("../controllers/user")
 const User = require("../models/user");
-
+const checkAuth = require("../middleware/check-auth");
 // sign up route
-router.post("/signup", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: "User already exists on this email address",
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err,
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-            });
-            user
-              .save()
-              .then((result) => {
-                console.log(result);
-                res.status(201).json({
-                  message: "user created",
-                });
-              })
-              .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                  error: err,
-                });
-              });
-          }
-        });
-      }
-    });
-});
+router.post("/signup", UserController.user_signup);
+
 // user login
-router.post("/login", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth failed",
-        });
-      }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth failed",
-          });
-        }
-        if (result) {
-          // token creation
-          const token = jwt.sign({
-            email: user[0],
-            userId: user[0].id,
-          }, 
-          process.env.JWT_KEY, 
-          {
-            expiresIn: "1h"
-          });
-          return res.status(200).json({
-            message: "Auth successful",
-            token: token
-          });
-        }
-        res.status(401).json({
-          message: "Auth failed",
-        });
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.post("/login", UserController.user_login);
 
 // delete user
-router.delete("/:userId", (req, res, next) => {
-  User.findOneAndRemove(req.params.id, (err) => {
-    if (err) {
-      res.status(500).json({
-        error: err,
-      });
-    } else {
-      res.status(200).json({
-        message: "user deleted successfully",
-      });
-    }
-  });
-});
+router.delete("/:userId", checkAuth, UserController.user_delete);
+
 module.exports = router;
