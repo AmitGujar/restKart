4cc0f0b6b046cc82677b40847f97c872b946cd8c
Amit Dilip Gujar <amithero3342@gmail.com>
2021-02-03 20:56:29 +0530

    delete user

--- api/routes/user.js
+++ api/routes/user.js
@@ -46,5 +46,29 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
-
+// delete user
+router.delete("/:userId", (req, res, next) => {
+  User.findOneAndRemove(req.params.id, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.status(200).json({
+        message: "user deleted successfully",
+      })
+    }
+  });
+  // .exec()
+  // .then(result => {
+  //   res.status(200).json({
+  //     message: "user deleted successfully",
+  //   })
+  // })
+  // .catch((err) => {
+  //   res.status(500).json({
+  //     error: err
+  //   });
+  // });
+});
 module.exports = router;
