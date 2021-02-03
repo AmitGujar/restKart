1b5ce536ec3da3a101c969ba262a3b5e946663e7
Amit Dilip Gujar <amithero3342@gmail.com>
2021-02-03 23:02:09 +0530

    token added

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -45,30 +46,60 @@ router.post("/signup", (req, res, next) => {
       }
     });
 });
+// user login
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign({
+            email: user[0],
+            userId: user[0].id,
+          }, 
+          process.env.JWT_KEY, 
+          {
+            expiresIn: "1h"
+          });
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      res.status(200).json({
+        message: "user deleted successfully",
+      });
+    });
+});
 
 // delete user
 router.delete("/:userId", (req, res, next) => {
   User.findOneAndRemove(req.params.id, (err) => {
     if (err) {
       res.status(500).json({
-        error: err
+        error: err,
       });
     } else {
       res.status(200).json({
         message: "user deleted successfully",
-      })
+      });
     }
   });
-  // .exec()
-  // .then(result => {
-  //   res.status(200).json({
-  //     message: "user deleted successfully",
-  //   })
-  // })
-  // .catch((err) => {
-  //   res.status(500).json({
-  //     error: err
-  //   });
-  // });
 });
 module.exports = router;
